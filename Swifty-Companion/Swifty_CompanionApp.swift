da4a5e82405b241bc72ffabedb08fff5ba4859ac
Jules <jbjulesbernard3@gmail.com>
2025-12-03 17:01:16 +0100

    init

---
+++ Swifty-Companion/Swifty_CompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_CompanionApp.swift
+//  Swifty-Companion
+//
+//  Created by jules bernard on 03.12.25.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_CompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
